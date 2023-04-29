952a663473f7eeb63a94bb2724196ed1173b3eb5
Abner Freitas <defreitasabner@gmail.com>
2023-04-28 22:18:53 -0300

    Typescript basic structure project.

---
+++ app/models/book.ts
@@ -0,0 +1,31 @@
+class Book {
+    private _title;
+    private _author;
+    private _genders;
+    private _pageNumber;
+
+    constructor(title, author, genders, pageNumber) {
+        this._title = title,
+        this._author = author,
+        this._genders = genders,
+        this._pageNumber = pageNumber
+    };
+
+    get title() {
+        return this.title;
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    get genders() {
+        return this._genders;
+    }
+
+    get pageNumber() {
+        return this._pageNumber;
+    }
+
+
+}
