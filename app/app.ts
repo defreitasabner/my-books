176489858c38a1876322c13bbd84ed2c8191ab80
Abner Freitas <defreitasabner@gmail.com>
2023-04-30 19:52:34 -0300

    Book controller interacting with form.

--- app/app.ts
+++ app/app.ts
@@ -1,12 +1,11 @@
-import { Book } from "./models/book.js";
-import { Books } from "./models/books.js";
+import { BookController } from "./controller/book_controller.js";
 
-const bookList = new Books();
-const book = new Book('Grande Sertão Veredas', 'Guimarães Rosa', ['nacional', 'ficção'], 700);
-const book2 = new Book('Clube da Luta', 'Chuck Palahniuk', ['ficção'], 200);
+const bookController = new BookController();
+const form = document.querySelector('[data-main-form]');
 
-bookList.add(book);
-bookList.add(book2);
-
-console.log(book);
-console.log(bookList.list());
+form.addEventListener(
+    'submit', event => {
+        event.preventDefault();
+        bookController.addNewBook();
+    }
+)
--- app/controller/book_controller.ts
+++ app/controller/book_controller.ts
@@ -1,13 +1,30 @@
+import { Book } from "../models/book.js";
+import { Books } from "../models/books.js";
+
 export class BookController {
     private inputTitle: HTMLInputElement;
     private inputAuthor: HTMLInputElement;
     private inputGenders: HTMLInputElement;
     private inputPageNumber: HTMLInputElement;
-    //TODO: Criar o formulário
+    private books: Books = new Books();
+
     constructor() {
-        this.inputTitle = document.querySelector('#title');
-        this.inputAuthor = document.querySelector('#author');
-        this.inputGenders = document.querySelector('#genders');
-        this.inputPageNumber = document.querySelector('#pageNumber');
+        this.inputTitle = document.querySelector('[data-title]');
+        this.inputAuthor = document.querySelector('[data-author]');
+        this.inputGenders = document.querySelector('[data-genders]');
+        this.inputPageNumber = document.querySelector('[data-page-number]');
     }
+
+    addNewBook(): void {
+        const listGendersTreated: string[] = this.inputGenders.value.split(',');
+        const pageNumberTreated = Number(this.inputPageNumber.value);
+        const newBook = new Book(
+            this.inputTitle.value,
+            this.inputAuthor.value,
+            listGendersTreated,
+            pageNumberTreated,
+        );
+        this.books.add(newBook);
+    }
+
 }
