e053ff84c6916036b02e3a64ad71542952bae25a
Abner Freitas <defreitasabner@gmail.com>
2023-06-21 17:19:01 -0300

    Started basic structure to project backend

---
+++ BooksAPI/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+
+using BookAPI.Models;
+
+namespace BookAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BookController : ControllerBase
+{
+
+    private static List<Book> books = new List<Book>();
+    private static int id = 0;
+
+    [HttpPost]
+     public IActionResult AddBook([FromBody] Book book)
+     {
+        book.Id = id++;
+        books.Add(book);
+        return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+        // Dessa forma padroniza com REST, informando no Header onde o recurso criado pode ser encontrado
+     }
+
+     [HttpGet]
+     public IEnumerable<Book> ListAllBooks([FromQuery] int skip = 0, [FromQuery]int take = 0)
+     {
+        return books.Skip(skip).Take(take);
+     }
+
+     [HttpGet("{id}")]
+     public IActionResult GetBookById(int id)
+     {
+        var book = books.FirstOrDefault(book => book.Id == id);
+        if(book == null) return NotFound();
+        return Ok(book);
+     }
+}
---
+++ BooksAPI/Models/Book.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookAPI.Models;
+
+public class Book
+{
+
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "The field 'title' is required")]
+    [MaxLength(200)]
+    public string Title { get; set; }
+    
+    [Required(ErrorMessage = "The field 'author' is required")]
+    [MaxLength(100)]
+    public string Author { get; set; }
+
+    [Required(ErrorMessage = "The field 'gender' is required")]
+    [MaxLength(100)]
+    public string Gender { get; set; }
+    
+    [Required]
+    [Range(0, Double.MaxValue, ErrorMessage = "The page number must be greater than 0")]
+    public int PageNumber { get; set; }
+}
