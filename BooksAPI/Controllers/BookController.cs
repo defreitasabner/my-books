9fb16db011efc18b4781521ed20eb63727801431
Abner Freitas <defreitasabner@gmail.com>
2023-06-27 18:24:23 -0300

    Fix file indetation

--- BooksAPI/Controllers/BookController.cs
+++ BooksAPI/Controllers/BookController.cs
@@ -21,54 +21,54 @@ public class BookController : ControllerBase
         _mapper = mapper;
    }
 
-      [HttpPost]
-      public IActionResult AddBook([FromBody] CreateBookDto bookDto)
-      {
-         Book book = _mapper.Map<Book>(bookDto);
-         _context.Books.Add(book);
-         _context.SaveChanges();
-         return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
-      }
+   [HttpPost]
+   public IActionResult AddBook([FromBody] CreateBookDto bookDto)
+   {
+      Book book = _mapper.Map<Book>(bookDto);
+      _context.Books.Add(book);
+      _context.SaveChanges();
+      return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+   }
 
-      [HttpGet]
-      public IActionResult ListAllBooks([FromQuery] int page = 1)
-      {
-         if(page > 0) {
-            int skip = 10 * (page - 1);
-            const int take = 10;
-            return Ok( 
-               _mapper.Map<List<ReadBookDto>>(_context.Books.Skip(skip).Take(take))
-            );
-         } else {
-            return BadRequest();
-         }
+   [HttpGet]
+   public IActionResult ListAllBooks([FromQuery] int page = 1)
+   {
+      if(page > 0) {
+         int skip = 10 * (page - 1);
+         const int take = 10;
+         return Ok( 
+            _mapper.Map<List<ReadBookDto>>(_context.Books.Skip(skip).Take(take))
+         );
+      } else {
+         return BadRequest();
       }
+   }
 
-      [HttpGet("{id}")]
-      public IActionResult GetBookById(int id)
-      {
-         var book = _context.Books.FirstOrDefault(book => book.Id == id);
-         if(book == null) return NotFound();
-         return Ok(book);
-      }
+   [HttpGet("{id}")]
+   public IActionResult GetBookById(int id)
+   {
+      var book = _context.Books.FirstOrDefault(book => book.Id == id);
+      if(book == null) return NotFound();
+      return Ok(book);
+   }
 
-      [HttpPut("{id}")]
-      public IActionResult UpdateBook(int id, [FromBody] UpdateBookDto bookDot)
-      {
-         var book = _context.Books.FirstOrDefault(book => book.Id == id);
-         if(book == null) return NotFound();
-         _mapper.Map(bookDot, book);
-         _context.SaveChanges();
-         return NoContent();
-      }
+   [HttpPut("{id}")]
+   public IActionResult UpdateBook(int id, [FromBody] UpdateBookDto bookDot)
+   {
+      var book = _context.Books.FirstOrDefault(book => book.Id == id);
+      if(book == null) return NotFound();
+      _mapper.Map(bookDot, book);
+      _context.SaveChanges();
+      return NoContent();
+   }
 
-      [HttpDelete("{id}")]
-      public IActionResult DeleteBook(int id)
-      {
-         var book = _context.Books.FirstOrDefault(book => book.Id == id);
-         if(book == null) return NotFound();
-         _context.Remove(book);
-         _context.SaveChanges();
-         return NoContent();
-      }
+   [HttpDelete("{id}")]
+   public IActionResult DeleteBook(int id)
+   {
+      var book = _context.Books.FirstOrDefault(book => book.Id == id);
+      if(book == null) return NotFound();
+      _context.Remove(book);
+      _context.SaveChanges();
+      return NoContent();
+   }
 }
