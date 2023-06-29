32cd47137da7b534a47998227d5448593ba4dfec
Abner Freitas <defreitasabner@gmail.com>
2023-06-29 11:31:20 -0300

    Fixed Genre model and DTOs validation

--- BooksAPI/Data/Dtos/CreateAuthorDto.cs
+++ BooksAPI/Data/Dtos/CreateAuthorDto.cs
@@ -5,6 +5,6 @@ namespace BooksAPI.Data.Dtos;
 public class CreateAuthorDto
 {
     [Required]
-    [StringLength(200, ErrorMessage = "Author's Name could have less than 200 characters.")]
+    [StringLength(200, ErrorMessage = "Author's 'name' couldn't have more than 200 characters.")]
     public string Name { get; set; }
 }
