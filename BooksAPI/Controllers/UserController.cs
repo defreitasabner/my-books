42664bdc7f87603590abc9dea95ff6523ac3c185
Abner Freitas <defreitasabner@gmail.com>
2023-07-10 15:02:50 -0300

    Implemented User login without Token yet.

--- BooksAPI/Controllers/UserController.cs
+++ BooksAPI/Controllers/UserController.cs
@@ -19,10 +19,17 @@ public class UserController : ControllerBase
         _authService = auth;
     }
 
-    [HttpPost]
+    [HttpPost("register")]
     public async Task<IActionResult> Register(CreateUserDto userDto)
     {
         await _authService.RegisterNewUser(userDto);
         return Ok("User registered successfully!");
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginUserDto userDto)
+    {
+        await _authService.LoginUser(userDto);
+        return Ok("User login successfully!");
     }
 }
--- BooksAPI/Services/AuthenticationService.cs
+++ BooksAPI/Services/AuthenticationService.cs
@@ -10,11 +10,17 @@ public class AuthenticationService
 {
     private IMapper _mapper;
     private UserManager<User> _userManager;
+    private SignInManager<User> _signinManager;
 
-    public AuthenticationService(IMapper mapper, UserManager<User> userManager)
+    public AuthenticationService(
+        IMapper mapper, 
+        UserManager<User> userManager, 
+        SignInManager<User> signinManager
+    )
     {
         _mapper = mapper;
         _userManager = userManager;
+        _signinManager = signinManager;
     }
 
     public async Task RegisterNewUser(CreateUserDto userDto)
@@ -22,5 +28,16 @@ public class AuthenticationService
         User user = _mapper.Map<User>(userDto);
         IdentityResult result = await _userManager.CreateAsync(user, userDto.Password);
         if(!result.Succeeded) throw new ApplicationException("Failed to register new user ...");
+    }
+
+    public async Task LoginUser(LoginUserDto userDto)
+    {
+        var result = await _signinManager.PasswordSignInAsync(
+            userDto.Username,
+            userDto.Password,
+            false,
+            false
+        );
+        if(!result.Succeeded) throw new ApplicationException("Failed to login");
     }
 }
